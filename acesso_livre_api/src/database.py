4099c8d727e9d394de3472d2147ca5b12e066021
wandrey7 <123254136+wandrey7@users.noreply.github.com>
2025-09-11 18:11:52 -0300

    feat(db): integrate SQLAlchemy for database connection

--- acesso_livre_api/src/database.py
+++ acesso_livre_api/src/database.py
@@ -1,1 +1,15 @@
-# Configuração do banco de dados
+from .config import settings
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+SQLALCHEMY_DATABASE_URL = settings.database_url
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- acesso_livre_api/src/main.py
+++ acesso_livre_api/src/main.py
@@ -1,8 +1,15 @@
 from typing import Union
 from fastapi import FastAPI
+from .database import Base, engine, get_db
 from .config import settings
+from .comments.router import router as comments_router
 
 app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+
+app.include_router(comments_router, prefix="/api/comments", tags=["comments"])
 
 @app.get("/")
 def read_root():
